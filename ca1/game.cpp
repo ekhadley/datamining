4d9b0445cc25cb28b2b9d180cc8630ea7a5fd23d
Ethan <ekhadley@gmail.com>
2025-01-29 21:15:49 -0600

    asd

---
+++ ca1/game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class GameState {
+  vector<vector<char>> squares;
+  
+  GameState(int size) {
+    vector<vector<char>> squares;
+    for (int x = 0; x < size*2; x += 1) {
+      squares.push_back(vector<char>());
+      for (int y = 0; y < size*2; y += 1) {
+        squares.at
+      }
+    }
+  }
+
+};
--- ca1/main.cpp
+++ ca1/main.cpp
@@ -1,1 +1,12 @@
-asd
+#include <iostream>
+#include "game.cpp"
+#include "searches.cpp"
+
+
+
+int main() {
+  int x = 123;
+  printf("hi %d\n", x);
+
+  return 0;
+}
